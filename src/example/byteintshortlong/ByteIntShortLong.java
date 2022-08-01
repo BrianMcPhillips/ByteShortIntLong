ef7cb3be3f286056e3a81276a40cf2a7838065f7
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-01 12:34:52 -0400

    Max byte and short value

--- src/example/byteintshortlong/ByteIntShortLong.java
+++ src/example/byteintshortlong/ByteIntShortLong.java
@@ -11,5 +11,17 @@ public class ByteIntShortLong {
         System.out.println("Integer Maximum Value = " + myMaxIntValue);
         System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
         System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
     }
 }
